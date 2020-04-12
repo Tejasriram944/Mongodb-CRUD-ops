e73a7e1b9c0a259e0494612d78d947eba475e91b
TEJA SRIRAM <49815246+Tejasriram944@users.noreply.github.com>
2020-04-12 19:19:22 +0530

    new files

---
+++ Demo1.js
@@ -0,0 +1,33 @@
+const MongoClient = require('mongodb').MongoClient;
+// const assert = require('assert');
+
+// Connection URL
+const url = 'mongodb://127.0.0.1:27017';
+
+// Database Name
+const dbName = 'infosys_db';
+
+// Create a new MongoClient
+const client = new MongoClient(url);
+
+// Use connect method to connect to the Server
+client.connect(function(err) {
+    // assert.equal(null, err);
+    console.log("Connected successfully to server ");
+    const db = client.db(dbName);
+
+    // insert a multiple document
+    let employes = [
+        {id:202, name: 'teja', age:24, location: 'GnT'},
+        {id:203, name: 'sriram', age:22, location: 'LONDAN'},
+        {id:204, name: 'founder', age:25, location: 'GEORGE'},
+        {id:205, name: 'creative', age:23, location: 'TNL'},
+
+    ];
+    db.collection('employe').insertMany(employes, function (err, r) {
+        if(err) throw err;
+        console.log('All records are inserted');
+    });
+
+    client.close();
+});
